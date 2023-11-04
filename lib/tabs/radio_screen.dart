76fef571750967bb94db64247faab9475a016866
Ahmed Mohmed <111065068+Ahmed7773333@users.noreply.github.com>
2023-11-04 23:39:30 +0200

    removing warnnigs

---
+++ lib/tabs/radio_screen.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:islamy_app/app_theme.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import '../assets.dart';
+import '../providers/l_d_mode.dart';
+
+class RadioScreen extends StatelessWidget {
+  const RadioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Color iconColor = Provider.of<ThemeProvider>(context).isDarkMode
+        ? const Color(0xffFACC1D)
+        : AppTheme.primaryColor;
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage(
+                Provider.of<ThemeProvider>(context).isDarkMode
+                    ? Assets().darkBackground
+                    : Assets().ligtBackground,
+              ),
+              fit: BoxFit.fill)),
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).size.height * (20 / 870),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                AppLocalizations.of(context)!.islamy,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * (173 / 870),
+              ),
+              SizedBox(
+                  height: MediaQuery.of(context).size.height * (222 / 870),
+                  width: MediaQuery.of(context).size.width * (412 / 412),
+                  child: Image.asset(Assets().radios)),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * (57 / 870),
+              ),
+              Text(
+                AppLocalizations.of(context)!.quranRadio,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * (57 / 870),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Spacer(
+                    flex: 3,
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.skip_previous_rounded,
+                      color: iconColor,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.play_arrow_rounded,
+                      color: iconColor,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.skip_next_rounded,
+                      color: iconColor,
+                      size: 40,
+                    ),
+                  ),
+                  const Spacer(
+                    flex: 3,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
